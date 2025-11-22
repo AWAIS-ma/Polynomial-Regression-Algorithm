83c516cf19de7e98ec870c61c34503a16272c449
Muhammad Awais <muhammadawaisafzal444@gmail.com>
2025-11-22 22:50:28 +0500

    Polynomial-Regression-Algorithm

--- data making.py
+++ data making.py
@@ -1,22 +1,18 @@
 import pandas as pd
 import numpy as np
 
-# -----------------------------
 # 1. DEFINE DATA RANGES
-# -----------------------------
-temperatures = np.arange(10, 41, 2)  # 10°C se 40°C tak, step=2
-humidity = np.linspace(85, 98, len(temperatures))  # Humidity % 85-98
-wind_speed = np.linspace(15, 21, len(temperatures))  # Wind Speed 15-21 km/h
+max_entries = 300
+temperatures = np.linspace(10, 40, max_entries)  # 10°C se 40°C 
+humidity = np.linspace(85, 98, max_entries)      # Humidity % 85-98
+wind_speed = np.linspace(15, 21, max_entries)   # Wind Speed 15-21 km/h
 
-# -----------------------------
 # 2. SIMULATE NON-LINEAR ELECTRICITY USAGE
 # -----------------------------
 # Usage decreases initially, then increases (U-shape curve)
 electricity_usage = 0.5*(temperatures-20)**2 + 50  # quadratic pattern
 
-# -----------------------------
 # 3. CREATE DATAFRAME
-# -----------------------------
 data = pd.DataFrame({
     "Temperature": temperatures,
     "Humidity": humidity.astype(int),
@@ -24,9 +20,7 @@ data = pd.DataFrame({
     "Electricity_Usage": electricity_usage.astype(int)
 })
 
-# -----------------------------
 # 4. SAVE CSV
-# -----------------------------
 data.to_csv("energy_data.csv", index=False)
 print("Dataset saved as 'energy_data.csv'\n")
 print(data)
